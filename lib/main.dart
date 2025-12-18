192c64d2ab1eff6f8b1618ec4b38221777de0ffc
XRedX <129623048+XRed8X@users.noreply.github.com>
2025-12-17 20:07:28 -0600

    Feat: Provider structure added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:vertical_videos/config/theme/app_theme.dart';
-import 'package:vertical_videos/presentation/providers/discover_provider.dart';
-import 'package:vertical_videos/presentation/screens/discover/discover_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:vertical_videos/presentation/providers/discover_provider.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,15 +12,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(
-          create: (_) =>DiscoverProvider(),
-        )
+        ChangeNotifierProvider(create: (context) => DiscoverProvider())
       ],
       child: MaterialApp(
         title: 'Vertical Videos',
         theme: AppTheme().getTheme(),
         debugShowCheckedModeBanner: false,
-        home: DiscoverScreen()
+        home: Scaffold(
+          appBar: AppBar(title: const Text('Material App Bar')),
+          body: const Center(child: Text('Hello World')),
+        ),
       ),
     );
   }
